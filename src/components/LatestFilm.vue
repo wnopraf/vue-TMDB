88b7dbdb4c2a5b266c03cc3f23a43a2e8fa3d807
wnpraf <rapribu@gmail.com>
2020-05-02 01:15:47 +0200

    Add LatestFilm component

---
+++ src/components/LatestFilm.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="latest-film">
+    <h3>{{title}}</h3>
+    <figure v-if="poster_path">
+      <img :src="poster_path" alt />
+    </figure>
+    <p v-else>No poster avaliable</p>
+
+    <p>{{overview}}</p>
+    <video v-if="video" :src="video"></video>
+    <p v-if="release-date">{{release_date}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: 'string',
+    overview: 'string',
+    poster_path: 'string',
+    release_date: 'string',
+    video: 'string'
+  }
+}
+</script>
