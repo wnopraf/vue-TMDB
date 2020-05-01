55d15487a5388043f3c5b37505da82d05d3f5576
wnpraf <rapribu@gmail.com>
2020-05-02 01:11:10 +0200

    Fix base axios url

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
-axios.defaults.baseURL = `https://api.themoviedb.org/3/movie/latest?api_key=a829d43db76411945dadd8c5eb781abb`
+axios.defaults.baseURL = 'https://api.themoviedb.org/3/'
 
 export { axios }
