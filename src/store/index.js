cdd363f0a718ebddd1fc6f0a945dee16449aa013
wnpraf <rapribu@gmail.com>
2020-05-01 18:03:24 +0200

    Update constants and mutations

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,26 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import types from './constants'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    latestFilm: {},
+    nowPlaying: [],
+    apiDetail: {},
   },
   mutations: {
+    [types.API_LATEST]: ({ latestFilm }, payload) => {
+      latestFilm = payload
+    },
+    [types.API_NOW_PLAYING]: ({ nowPlaying }, payload) => {
+      nowPlaying = payload
+    },
+    [types.API_DETAIL]: ({ apiDetail }, payload) => {
+      apiDetail = payload
+    },
   },
-  actions: {
-  },
-  modules: {
-  }
+  actions: {},
+  modules: {},
 })
