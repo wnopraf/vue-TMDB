1d608ae183878a8ecb338e222aa0be70958ddf51
wnpraf <rapribu@gmail.com>
2020-05-02 14:06:41 +0200

    Add FilmItem component

---
+++ src/components/FilmItem.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="film-item">
+    <figure v-if="posterPath">
+      <img :src="process.env.VUE_APP_IMG_URL_LOGO + posterPath" alt />
+    </figure>
+    <div class="film-item__info">
+      <p>{{title}}</p>
+      <router-link :to="`movie/${id}`">view film</router-link>
+    </div>
+    <p v-if="releaseDate">{{releaseDate}}</p>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    title: {
+      type: String
+    },
+    posterPath: {
+      type: String
+    },
+    id: {
+      type: Number
+    },
+    releaseDate: {
+      type: String
+    }
+  }
+}
+</script>
