c3b807ad9af3c06febdc2ad8eb00dd4535ff0097
wnpraf <rapribu@gmail.com>
2020-05-03 00:00:38 +0200

    Add fetchnowplaying action to action stores

--- src/store/index.js
+++ src/store/index.js
@@ -10,7 +10,8 @@ export default new Vuex.Store({
     latestFilm: {},
     nowPlaying: [],
     apiDetail: {},
-    fetchStatus: false
+    fetchLatestStatus: false,
+    fetchNowPlayingStatus: false
   },
   mutations: {
     [types.API_LATEST]: (store, { data }) => {
@@ -23,36 +24,47 @@ export default new Vuex.Store({
     [types.API_DETAIL]: (store, payload) => {
       store.apiDetail = payload
     },
-    [types.FETCH_END]: (store, payload) => {
-      store.fetchStatus = false
+    [types.FETCH_LATEST_END]: store => {
+      store.fetchLatestStatus = false
     },
-    [types.FETCH_START]: (store, payload) => {
-      store.fetchStatus = true
+    [types.FETCH_LATEST_START]: store => {
+      store.fetchLatestStatus = true
+    },
+    [types.FETCH_NOW_PLAYING_START]: store => {
+      store.fetchNowPlayingStatus = true
+    },
+    [types.FETCH_NOW_PLAYING_END]: store => {
+      store.fetchNowPlayingStatus = false
     }
   },
   actions: {
     fetchLatest: async ({ commit }) => {
       try {
-        commit('FETCH_START')
+        commit(types.FETCH_LATEST_START)
         const latestFilm = await axios.get(
           `movie/latest?api_key=${process.env.VUE_APP_API_KEY}`
         )
         console.log(latestFilm, 'fetch latest')
-        commit('FETCH_END')
+        commit(types.FETCH_LATEST_END)
         commit(types.API_LATEST, latestFilm)
       } catch (error) {
         // catch error depending of status code response error
         console.log(error)
       }
     },
-    fetchNowPlaying: async ({ commit }, payload) => {
-      payload = payload || 1
-      commit('FETCH_NOW_PLAYING_START')
-      const nowPlaying = await axios.get(
-        `movie/now_playing?api_key=${process.env.VUE_APP_API_KEY}&page=${payload}`
-      )
-      commit('FETCH_NOW_PLAYING_END')
-      commit(types.API_NOW_PLAYING, nowPlaying)
+    fetchNowPlaying: async ({ commit }, { page } = { page: 1 }) => {
+      try {
+        commit(types.FETCH_NOW_PLAYING_START)
+        const { data: nowPlaying } = await axios.get(
+          `movie/now_playing?api_key=${process.env.VUE_APP_API_KEY}&page=${page}`
+        )
+        console.log(nowPlaying, 'nowplaying')
+        commit(types.FETCH_NOW_PLAYING_END)
+        commit(types.API_NOW_PLAYING, nowPlaying)
+      } catch (error) {
+        // catch error depending of status code response error
+        console.log(error, 'fetch_now_playing_error')
+      }
     }
   },
   modules: {}
