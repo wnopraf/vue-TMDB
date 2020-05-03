e69920a9a9a903662bd84343d8ba4a4983b7a79f
wnpraf <rapribu@gmail.com>
2020-05-03 17:08:28 +0200

    Add markup structurre and prop validation

--- src/components/FilmDetail.vue
+++ src/components/FilmDetail.vue
@@ -2,7 +2,7 @@
   <div class="film-detail">
     <div class="film-detail__film-info">
       <figure>
-        <img :src="posterPath" alt />
+        <img :src="globalImagePosterPath + posterPath" alt />
       </figure>
       <h1>{{title}}</h1>
       <p>{{overview}}</p>
@@ -12,7 +12,7 @@
       <ul>
         <li v-for="(actor, key) in credits.cast" :key="key">
           <figure>
-            <img :src="actor.profilePath" alt />
+            <img :src="globalImgLogoPath + actor.profilePath" alt />
           </figure>
           <div class="cast__info">
             <span>Actor Name</span>
@@ -24,8 +24,53 @@
       </ul>
     </div>
     <div class="film-detail__ratings">
-      <p>Vote count: {{voteCount}}</p>
-      <p>Vote average: {{voteAverage}}</p>
+      <div class="film-detail__ratings__vote-rate">
+        <p>Vote count: {{voteCount}}</p>
+        <p>Vote average: {{voteAverage}}</p>
+      </div>
+      <p class="film-detail__ratigns__popularity">
+        <span>Popularity:</span>
+        <span>{{popularity}}</span>
+      </p>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String
+    },
+    releasDate: {
+      type: String
+    },
+    overview: {
+      type: String
+    },
+    posterPath: {
+      type: String
+    },
+    globalImagePosterPath: {
+      type: String,
+      default: process.env.VUE_APP_IMG_URL_POSTER
+    },
+    globalImgLogoPath: {
+      type: String,
+      default: process.env.VUE_APP_IMG_URL_LOGO
+    },
+    credits: {
+      type: Object
+    },
+    voteCout: {
+      type: Number
+    },
+    voteAverage: {
+      type: Number
+    },
+    popularity: {
+      type: Number
+    }
+  }
+}
+</script>
