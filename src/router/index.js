cf3f54a2641327e350c321a9df2737436f81ff53
wnpraf <rapribu@gmail.com>
2020-05-03 17:33:02 +0200

    Add route param trough route conf

--- src/router/index.js
+++ src/router/index.js
@@ -11,8 +11,9 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/movie/:id',
+    name: 'Detail',
+    props: ['id'],
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -3,3 +3,24 @@
     <h1>This is an about page</h1>
   </div>
 </template>
+<script>
+import FilmDetail from '@/components/FilmDetail'
+export default {
+  props: {
+    id: {
+      type: Number
+    }
+  },
+  components: {
+    FilmDetail
+  },
+  computed: {
+    film() {
+      return this.$store.state.FilmDetail
+    }
+  },
+  created() {
+    this.$store.dispatch('fetchDetail', id)
+  }
+}
+</script>
