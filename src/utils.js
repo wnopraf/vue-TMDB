015cac29da0d53f1067019071a737384860e3e68
wnpraf <rapribu@gmail.com>
2020-05-03 13:39:22 +0200

    Add api error handler

--- src/utils.js
+++ src/utils.js
@@ -2,4 +2,13 @@ import axios from 'axios'
 
 axios.defaults.baseURL = 'https://api.themoviedb.org/3/'
 
-export { axios }
+function handleResError(statusCode, commit) {
+  switch (statusCode) {
+    case 500:
+      return commit(types.ERROR_500)
+    case 401:
+      return commit(types.ERROR_401)
+  }
+}
+
+export { axios, handleResError }
