e29af8f5ada492676597c1d9554684964f80c062
wnpraf <rapribu@gmail.com>
2020-05-05 21:25:47 +0200

    Update error handler api calls function

--- src/utils.js
+++ src/utils.js
@@ -1,13 +1,17 @@
 import axios from 'axios'
 
 axios.defaults.baseURL = 'https://api.themoviedb.org/3/'
-
-function handleResError (statusCode, commit, types) {
+/* 
+ function in progess implementation that manages all
+ api error calls in client side through displaying
+ pertinent error response views.
+*/
+function handleResError(commit, $router) {
   switch (statusCode) {
     case 500:
-      return commit(types.ERROR_500)
+      return commit(types.ERROR_500, { $router })
     case 401:
-      return commit(types.ERROR_401)
+      return commit(types.ERROR_401, { $router })
   }
 }
 
