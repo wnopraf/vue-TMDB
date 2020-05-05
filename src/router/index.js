e8e5df982268414c71cbc68a61f584c974c06f3d
wnpraf <rapribu@gmail.com>
2020-05-05 17:37:23 +0200

    Add 404 route error

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Detail from '../views/Detail.vue'
+import Error404 from '../views/404.vue'
 
 Vue.use(VueRouter)
 
@@ -16,6 +17,10 @@ const routes = [
     name: 'Detail',
     component: Detail,
     props: true
+  },
+  {
+    path: '/*',
+    component: Error404
   }
 ]
 
---
+++ src/views/404.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="error-404 py-56">
+    <h1>Error 404</h1>
+    <p class="text-xl">Sorry, the requested route does not exit : (</p>
+  </div>
+</template>
